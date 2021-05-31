5a80482098afa27a4e9f1d57d234726ddcd626f3
汤瑞 <rtang@che300.com>
2021-05-31 14:20:09 +0800

    5-30 杂  泛型待添加  测试代码

---
+++ app/src/main/java/com/person/kotlintest/generic_paradigm/Test.java
@@ -0,0 +1,33 @@
+package com.person.kotlintest.generic_paradigm;
+
+/**
+ * @anthor tr
+ * @date 2021/5/28
+ * @desc
+ */
+public class Test {
+
+    // 泛型方法 printArray
+    public static <E> void printArray(E[] input) {
+        for (E element : input) {
+            System.out.printf("%s ", element);
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        // 创建不同类型数组： Integer, Double 和 Character
+        Integer[] intArray = {1, 2, 3, 4, 5};
+        Double[] doubleArray = {1.1, 2.2, 3.3, 4.4};
+        Character[] charArray = {'H', 'E', 'L', 'L', 'O'};
+
+        System.out.println("整型数组元素为:");
+        printArray(intArray); // 传递一个整型数组
+
+        System.out.println("\n双精度型数组元素为:");
+        printArray(doubleArray); // 传递一个双精度型数组
+
+        System.out.println("\n字符型数组元素为:");
+        printArray(charArray); // 传递一个字符型数组
+    }
+}
